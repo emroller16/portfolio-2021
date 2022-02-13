a5a6f6dbc12d24e90a4093aeca3ed0656e8ec696
Emily Roller <emroller16@gmail.com>
2022-02-13 08:24:41 -0700

    Update home-page-grid.js

--- src/components/home-page-grid.js
+++ src/components/home-page-grid.js
@@ -19,7 +19,6 @@ export default class HomePageGrid extends Component {
           name: 'Design System 2.0',
           type: 'Design Systems',
           link: 'https://www.figma.com/proto/gypKJeGAe49YQxQ84EWojH/Emily's-Portfolio?node-id=831%3A2299&scaling=contain&page-id=7%3A261&starting-point-node-id=831%3A2299',
-          // status: 'Coming soon!',
         },
         {
           name: 'Team processes',
